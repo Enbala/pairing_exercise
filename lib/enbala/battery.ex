ce9cd9d905d271a0a0ba3458e1e5ae59168b9269
Tony Winn <twinn@enbala.com>
2021-02-17 15:18:46 -0500

    English

--- lib/enbala/battery.ex
+++ lib/enbala/battery.ex
@@ -4,9 +4,9 @@ defmodule Enbala.Battery do
   the grid (via a smart inverter) is a really helpful tool to be able to leverage.
   They're capable of exporting power to the grid if there isn't enough generation,
   absorb power if there's too much generation, and even regulate frequency (and
-  that's not all!). 
+  that's not all!).
 
-  Our battery module is a little bit simpler then real-world batteries. We're only
+  Our battery module is a little bit simpler than real-world batteries. We're only
   interested in keeping track of three things for our batteries. A unique `id`
   that we can use to send it messages, the amount of power the battery is currently
   contributing to the grid (current_power), and what the maxim amount of power the
