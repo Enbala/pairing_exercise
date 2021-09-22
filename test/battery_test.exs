84654acfe9a00c48c587611d430aefed921bfb7a
Tyler Young <tyoung@enbala.com>
2021-09-22 13:50:58 -0500

    Change the APIs and tests to provide more direction on refactorings

--- test/battery_test.exs
+++ test/battery_test.exs
@@ -10,39 +10,34 @@ defmodule Enbala.BatteryTest do
   end
 
   @tag :skip
-  test ".create takes a map of parameters, builds a struct and persists" do
+  test ".new takes a map of parameters and builds a struct" do
     battery_params = %{id: "battery_1"}
 
-    assert {:ok, %Battery{}} = Battery.create(battery_params)
+    assert {:ok, %Battery{}} = Battery.new(battery_params)
   end
 
   @tag :skip
-  test ".get with a previously created id retrieves the struct" do
-    id = "battery_1"
-    Battery.create(%{id: id})
+  test ".get returns a previously created battery struct" do
+    {:ok, created} = Battery.new(%{id: "b1", current_power: 50, rated_power: 100})
 
-    assert %Battery{id: ^id} = Battery.get(id)
+    assert Battery.get("b1") == created
+  end
+
+  test ".new does basic validations" do
+    # TODO: What should we do here?
+    assert {:error, _} = Battery.new(%{id: nil})
+
+    # TODO: Should we enforce current_power <= rated_power?
+    assert {:error, _} = Battery.new(%{id: "b1", current_power: 999, rated_power: 10})
+
+    # TODO: Other validations?
   end
 
   @tag :skip
-  test ".get with an id that hasn't been created" do
-    assert nil == Battery.get("unknown_id")
-  end
+  test ".update_current_power/2" do
+    {:ok, battery} = Battery.new(%{id: "b1", current_power: 100, rated_power: 1_000})
+    updated_battery = Battery.set_current_power("b1", 50)
 
-  @tag :skip
-  test ".all returns battery structs that have been created" do
-    id = "battery_1"
-    Battery.create(%{id: id})
-
-    assert [%Battery{id: ^id}] = Battery.all()
-  end
-
-  @tag :skip
-  test ".set_current_power/2" do
-    id = "battery_1"
-    Battery.create(%{id: id})
-    Battery.set_current_power(id, 5)
-
-    assert %Battery{current_power: 5} = Battery.get(id)
+    assert updated_battery.current_power == 50
   end
 end
