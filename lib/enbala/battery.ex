5842cf908906db1d7fd93fdfe71b978e8802c486
Bijan Boustani <bboustani@enbala.com>
2021-10-18 12:19:47 -0400

    Set 80 character line limit for moduledoc text

--- lib/enbala/battery.ex
+++ lib/enbala/battery.ex
@@ -1,16 +1,16 @@
 defmodule Enbala.Battery do
   @moduledoc """
   When Enbala is managing the health of the power grid, a battery connected to
-  the grid (via a smart inverter) is a really helpful tool to be able to leverage.
-  They're capable of exporting power to the grid if there isn't enough generation,
-  absorb power if there's too much generation, and even regulate frequency (and
-  that's not all!).
+  the grid (via a smart inverter) is a really helpful tool to be able to
+  leverage. They're capable of exporting power to the grid if there isn't
+  enough generation, absorb power if there's too much generation, and even
+  regulate frequency (and that's not all!).
 
-  Our battery module is a little bit simpler than real-world batteries. We're only
-  interested in keeping track of three things for our batteries. A unique `id`
-  that we can use to send it messages, the amount of power the battery is currently
-  contributing to the grid (current_power), and what the maximum amount of power the
-  battery could contribute to the grid (rated_power).
+  Our battery module is a little bit simpler than real-world batteries. We're
+  only interested in keeping track of three things for our batteries. A unique
+  `id` that we can use to send it messages, the amount of power the battery is
+  currently contributing to the grid (current_power), and what the maximum
+  amount of power the battery could contribute to the grid (rated_power).
   """
   defstruct [:id, :current_power, rated_power: 10]
 
--- lib/enbala/vpp.ex
+++ lib/enbala/vpp.ex
@@ -1,12 +1,13 @@
 defmodule Enbala.Vpp do
   @moduledoc """
-  A Virtual Power Plant or VPP is a collection of controllable assets, batteries in our
-  case, that we can use to bring additional stability to the grid.
+  A Virtual Power Plant or VPP is a collection of controllable assets,
+  batteries in our case, that we can use to bring additional stability to the
+  grid.
 
-  Our Vpp module is simpler than what would be used in the real-world, it can only do
-  two things. Firstly, it can calculate the total amount of power all of our batteries
-  are contributing to the grid. Secondly, it can be asked to export an additional amount
-  of power to the grid.
+  Our Vpp module is simpler than what would be used in the real-world, it can
+  only do two things. Firstly, it can calculate the total amount of power all
+  of our batteries are contributing to the grid. Secondly, it can be asked to
+  export an additional amount of power to the grid.
   """
 
   def add_asset(battery) do
